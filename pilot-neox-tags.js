0e3f48cd39880065513dce05fc8b971800b7b8ad
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-02 10:20:46 +0900

    Add Pilot Neox hashtag rules

---
+++ pilot-neox-tags.js
@@ -0,0 +1,8 @@
+(() => {
+  const items = window.BLUEBLACK_JULY_PRODUCTS || [];
+  const item = items.find((entry) => entry.key === 'pilot-neox-wakaru');
+  if (!item) return;
+  item.content.hashtags = '#파이롯트 #PILOT #네옥스그래파이트 #NEOXGraphite #WAKARU #와카루 #샤프심 #샤프심추천 #샤프펜슬 #샤프리필 #03mm샤프심 #05mm샤프심 #한정문구 #문구신제품 #문구추천 #필기구 #필기구추천 #공부문구 #블루블랙펜샵 #블블샵';
+  item.hashtagTarget = 20;
+  item.officialAccountRequired = true;
+})();
